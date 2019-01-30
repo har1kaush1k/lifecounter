b32c928bd60fff82b2de816f5b12128d00fd5997
harik98 <harikaushik@gmail.com>
2019-01-29 16:29:50 -0800

    Initial Commit

---
+++ lifecounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lifecounter
+//
+//  Created by Hari Kaushik on 1/29/19.
+//  Copyright © 2019 Hari Kaushik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
